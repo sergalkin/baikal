e24df03790526e2b1b69503367b79621ea5a6e7b
sergalkin <sa.galkin94@gmail.com>
2019-02-04 06:09:21 +0300

    Finished landing

---
+++ js/control.js
@@ -0,0 +1,42 @@
+$(function () {
+
+    "use strict";
+
+    $('.title_box').click(function () {
+        $(this).toggleClass('open');
+        $(this).next('.list_link').toggleClass('open');
+    });
+
+    $('#slider_price').slider({
+        max: 1000,
+        min: 0,
+        range: true,
+        values: [190, 728],
+        slide: function (event, ui) {
+            $('input[name="minPrice"]').val('$' + ui.values[0]);
+            $('input[name="maxPrice"]').val('$' + ui.values[1]);
+        }
+    });
+
+    $('input[name="minPrice"]').val('$' + $('#slider_price').slider('values',0));
+    $('input[name="maxPrice"]').val('$' + $('#slider_price').slider('values',1));
+
+    $('#cart, .title_cart').click(function () {
+        $('#cart_box').toggleClass('open');
+    });
+
+    $('#sign').click(function () {
+        $('#black_fill').toggleClass('open');
+        $('#modal').toggleClass('open');
+    });
+
+    $('#close_mod, #black_fill').click(function () {
+        $('#black_fill').toggleClass('open');
+        $('#modal').toggleClass('open');
+    });
+
+    $('#search_button').click(function () {
+        $('#search_panel').toggleClass('open');
+    })
+
+});
